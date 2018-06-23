619a8887932eeeada84095c98484f8514a63af3a
Tomaszunek <pawlowskitomaszu@gmail.com>
2018-06-23 11:43:51 +0200

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import './App.css';
+import Navigation from './components/navigation/Navigation';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div>
+        <Navigation/>
+        <div className="container">
+        <h1>My Website</h1>
+      </div>
+      </div>      
+      
+    );
+  }
+}
+
+export default App;
---
+++ src/components/navigation/Navigation.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import './navigation.css';
+
+class Navigation extends React.Component {
+  public render() {
+    return (      
+        <nav className="navbar navbar-expand-lg">
+        <div className="viewport">
+            <a className="navbar-brand" href="/">PROHUMAN GAMES</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon">a</span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto">
+                <li className="nav-item active">
+                    <a className="nav-link" href="/">NEWS <span className="sr-only">(current)</span></a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="/">PROJECTS</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="/">SHOP</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="/">ABOUT</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="/">LOG IN</a>
+                </li>
+            </ul>
+            </div>
+        </div>        
+      </nav>
+    )
+  }
+}
+   
+
+export default Navigation;
